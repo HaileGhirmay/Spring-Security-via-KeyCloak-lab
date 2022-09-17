557b62e1f801271477bbdf889cf5282909e18a62
Haile@miu <hgpencilpower@gmail.com>
2022-09-17 10:52:18 -0500

    project setup first commit

---
+++ Spring-security-via-Keycloak/Spring-security-via-Keycloak/src/main/java/com/ultrates/SpringsecurityviaKeycloak/SpringSecurityViaKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.ultrates.SpringsecurityviaKeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityViaKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityViaKeycloakApplication.class, args);
+	}
+
+}
